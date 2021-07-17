098ed0d86263c03eb3fd1fe71e5667f7abfbb781
NguyenVanThinh2000 <18521448@gm.uit.edu.vn>
2021-07-17 17:09:53 +0700

    update

--- config/db/index.js
+++ config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect(){
     try {
-        await mongoose.connect('mongodb+srv://ThinhNguyen:Thinh2701@cluster0.rfhgh.mongodb.net/Sea-Furniture?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb://localhost:27017/Sea-Furniture' || 'mongodb+srv://ThinhNguyen:Thinh2701@cluster0.rfhgh.mongodb.net/Sea-Furniture?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
